6e674830ede784cdba3daf86e3be3b696502a586
Yisu Kim <yisu.kim.dev@outlook.com>
2024-07-24 17:18:41 +1000

    [feature] Update PostCard component to use shadcn-ui Card components
    - Removed thumbnail from the PostCard component

--- components/post-card.tsx
+++ components/post-card.tsx
@@ -1,9 +1,14 @@
 import Link from "next/link";
-import Image from "next/image";
 
 import { Locale } from "@/utils/i18n";
 import { dateFormatOptions } from "@/utils/date";
 import { Metadata } from "@/db/post";
+import {
+  Card,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 
 type Props = Readonly<{
   lang: Locale;
@@ -14,27 +19,25 @@ type Props = Readonly<{
 export default function PostCard({
   lang,
   slug,
-  metadata: { title, description, createdAt, thumbnail },
+  metadata: { title, description, createdAt },
 }: Props) {
   return (
     <Link href={`/${lang}/posts/${slug}`}>
-      <article className="h-full rounded-lg border p-4">
-        <div className="flex h-full flex-col gap-4">
-          <Image
-            className="w-full rounded-lg border"
-            src={`/static/posts/${slug}/${thumbnail}`}
-            alt={title}
-            width={400}
-            height={210}
-          />
-          <div className="flex grow flex-col">
-            <h2 className="text-xl font-bold">{title}</h2>
-            <p className="mt-1 w-full grow text-gray-500">{description}</p>
-            <time className="mt-4 self-end text-sm text-gray-500">
-              {new Date(createdAt).toLocaleDateString(lang, dateFormatOptions)}
-            </time>
-          </div>
-        </div>
+      <article className="h-full">
+        <Card className="flex h-full flex-col">
+          <CardHeader className="flex grow flex-col">
+            <CardTitle className="text-xl font-bold">{title}</CardTitle>
+            <CardDescription className="grow">{description}</CardDescription>
+            <CardDescription className="flex">
+              <time className="ml-auto">
+                {new Date(createdAt).toLocaleDateString(
+                  lang,
+                  dateFormatOptions,
+                )}
+              </time>
+            </CardDescription>
+          </CardHeader>
+        </Card>
       </article>
     </Link>
   );
