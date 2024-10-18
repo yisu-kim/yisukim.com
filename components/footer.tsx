555d1153cb2b87cbaccdec74b55a5e7617097ee2
Yisu Kim <yisu.kim.dev@outlook.com>
2024-10-18 16:01:06 +1100

    [fix] Update footer links to anchor tags with noreferrer for security

--- components/footer.tsx
+++ components/footer.tsx
@@ -23,27 +23,34 @@ export default function Footer({
     <footer className="mt-4 flex flex-col gap-2 border-t p-4 text-center">
       <div className="m-auto flex justify-between gap-2">
         <Button variant="ghost" size="icon" asChild>
-          <Link href="mailto:yisu.kim.job@outlook.com" aria-label={mail}>
+          <a
+            href="mailto:yisu.kim.job@outlook.com"
+            target="_blank"
+            rel="noreferrer"
+            aria-label={mail}
+          >
             <Email className="h-[1.2rem] w-[1.2rem]" />
-          </Link>
+          </a>
         </Button>
         <Button variant="ghost" size="icon" asChild>
-          <Link
+          <a
             href="https://github.com/yisu-kim"
             target="_blank"
+            rel="noreferrer"
             aria-label={github}
           >
             <GitHub className="h-[1.2rem] w-[1.2rem]" />
-          </Link>
+          </a>
         </Button>
         <Button variant="ghost" size="icon" asChild>
-          <Link
+          <a
             href="https://www.linkedin.com/in/yisu-kim"
             target="_blank"
+            rel="noreferrer"
             aria-label={linkedin}
           >
             <LinkedIn className="h-[1.2rem] w-[1.2rem]" />
-          </Link>
+          </a>
         </Button>
       </div>
       <p className="text-sm">
